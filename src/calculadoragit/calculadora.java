c16f10bd7659a60bcd060a1e844d333333595a13
Juan <daw03.2023@gmail.com>
2023-01-23 12:13:49 +0100

    Funcion Multiplicar

--- src/calculadoragit/calculadora.java
+++ src/calculadoragit/calculadora.java
@@ -4,4 +4,13 @@ public class calculadora {
 	public int sumar(int a, int b) {
 		return a + b;
 		}
+	public int restar(int a, int b) {
+		return a - b;
+		}
+	public int multiplicar(int a, int b) {
+		return a * b;
+		}
+	public int dividir(int a, int b) {
+		return a % b;
+		}
 }
--- src/calculadoragit/principal.java
+++ src/calculadoragit/principal.java
@@ -3,5 +3,10 @@ package calculadoragit;
 public class principal {
 public static void main(String[] args) {
 	calculadora miCalc=new calculadora();
+	System.out.println(miCalc.sumar(5, 3));
+	System.out.println(miCalc.restar(5, 3));
+	System.out.println(miCalc.multiplicar(5, 3));
+
+
 }
 }
