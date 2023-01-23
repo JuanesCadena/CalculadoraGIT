2e632a5e0930fdd5501b36ad4cb87c6f3857f916
JuanesCadena <122276319+JuanesCadena@users.noreply.github.com>
2023-01-23 12:26:18 +0100

    Función Dividir

--- src/calculadoragit/principal.java
+++ src/calculadoragit/principal.java
@@ -6,7 +6,6 @@ public static void main(String[] args) {
 	System.out.println(miCalc.sumar(5, 3));
 	System.out.println(miCalc.restar(5, 3));
 	System.out.println(miCalc.multiplicar(5, 3));
-
-
-}
+	System.out.println(miCalc.dividir(5, 3));
+    }
 }
